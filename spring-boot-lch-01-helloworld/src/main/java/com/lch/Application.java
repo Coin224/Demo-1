4da32477c9883dbda909cce43093aef4dcef3e2e
Coin224 <821504961@qq.com>
2020-03-20 12:13:49 +0800

    Hello World 第一次提交

---
+++ spring-boot-lch-01-helloworld/src/main/java/com/lch/Application.java
@@ -0,0 +1,16 @@
+package com.lch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 声明这是入口类
+ */
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ spring-boot-lch-01-helloworld/src/main/java/com/lch/controller/DemoController.java
@@ -0,0 +1,25 @@
+package com.lch.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+/**
+ * 声明这是一个控制类
+ * 主要负责业务逻辑 请求分发 。。。
+ */
+@Controller
+public class DemoController {
+
+
+    /**
+     * 请求名字与方法名字的映射来找到这个方法
+     * @return
+     */
+    @RequestMapping("/hello")
+    @ResponseBody//代表返回的是一个JSON
+    public String test() {
+        return "Hello World";
+    }
+}
